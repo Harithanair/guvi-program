424e4c6ff4fc7f330a31e320b034437c35b23bbc
Haritha.H <chinjuharitha23@gmail.com>
2017-06-07 23:28:21 +0530

    Create camel cse.c

---
+++ camel cse.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+char arr1[100];
+int f,i,l;
+printf("Enter the sentences\n");
+gets(arr1);
+for(i=0;l<arr[l]!='\0';l++);
+if(arr1[0]>='a'&&arr1[0]<='z'){
+arr1[0]=arr1[0]-32;
+}
+for(i=1;i<l;i++)
+{
+if(arr1[i]==''){
+if(arr1[i+1]>='a'&&arr[i+1]<='z'){
+int j=i+1;
+arr1[j]=arr[j]-32;
+}
+}
