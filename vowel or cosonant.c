4bc4b6ff167d49279dfb0ec527256dc71f5b03e0
Haritha.H <chinjuharitha23@gmail.com>
2017-06-08 11:19:34 +0530

    Create vowel or cosonant.c

---
+++ vowel or cosonant.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char c;
+int isLowercaseVowel,isUppercaseVowel;
+printf("enter the alphabet");
+scand("%c",&c);
+if isLowercaseVowel=(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+if isUppercaseVowel=(c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+if(isLowercaseVowel || isUppercaseLower)
+printf("%c is a vowel",c);
+else
+printf("%c is a consonant",c);
+return 0;
+}
